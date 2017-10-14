832d54eb2fe24d00a38fd47d9e7ed6577ddbee07
Dmitriy Batogov <hi@batogov.ru>
2017-10-14 19:46:31 +0300

    Изменил класс SpriteSheet

--- src/js/sprite-sheet.js
+++ src/js/sprite-sheet.js
@@ -1,56 +1,75 @@
 class SpriteSheet {
-    constructor(image, width, height) {
-        // Исходный спрайт
+    constructor(image, tileWidth, tileHeight) {
+        // Исходное изображение
         this.image = image;
 
         // Ширина и высота тайлов
-        this.width = width;
-        this.height = height;
+        this.tileWidth = tileWidth;
+        this.tileHeight = tileHeight;
 
-        // Коллекция определённых тайлов
-        this.tiles = new Map();
+        // Коллекция определённых спрайтов
+        this.sprites = new Map();
     }
 
     /**
-     * Функция определяет в спрайт-листе новый тайл.
-     * @param {string} name Имя тайла.
-     * @param {number} x Позиция тайла по горизонтали в исходном спрайте.
-     * @param {number} y Позиция по вертикали.
+     * Функция определяет в спрайт-листе новый спрайт.
+     * @param {string} name Имя спрайта.
+     * @param {number} x Координата спрайта по горизонтали в исходном изображении.
+     * @param {number} y Координата по вертикали.
+     * @param {number} width Ширина спрайта.
+     * @param {number} width Высота спрайта.
      */
-    define(name, x, y) {
+    define(name, x, y, width, height) {
         const buffer = document.createElement('canvas');
 
-        buffer.width = this.width;
-        buffer.height = this.height;
+        buffer.width = width;
+        buffer.height = height;
 
         buffer.getContext('2d').drawImage(
             this.image,
-            x * this.width,
-            y * this.height,
-            this.width,
-            this.height,
+            x,
+            y,
+            width,
+            height,
             0,
             0,
-            this.width,
-            this.height
+            width,
+            height
         );
 
-        this.tiles.set(name, buffer);
+        this.sprites.set(name, buffer);
+    }
+
+    /**
+     * Функция определяет в спрайт-листе новый тайл (спрайт с определённой
+     * шириной и высотой).
+     * @param {*} name Имя тайла.
+     * @param {*} x Позиция тайла по горизонтали.
+     * @param {*} y Позиция тайла по вертикали.
+     */
+    defineTile(name, x, y) {
+        this.define(
+            name,
+            x * this.tileWidth,
+            y * this.tileHeight,
+            this.tileWidth,
+            this.tileHeight
+        );
     }
 
     /**
      * Функция рисует тайл из спрайт-листа.
      * @param {string} name Имя тайла.
      * @param {object} context Контекст рисования.
-     * @param {number} x Координата по горизонтали.
-     * @param {number} y Координата по вертикали.
+     * @param {number} x Позиция тайла по горизонтали.
+     * @param {number} y Позиция тайла по вертикали.
      */
     drawTile(name, context, x, y) {
-        this.draw(name, context, x * this.width, y * this.height);
+        this.draw(name, context, x * this.tileWidth, y * this.tileHeight);
     }
 
     draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
+        const buffer = this.sprites.get(name);
         context.drawImage(buffer, x, y);
     }
 }
