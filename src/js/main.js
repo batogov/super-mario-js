3845c296e7607d686fe879c7381a44b1905ab12e
Dmitriy Batogov <hi@batogov.ru>
2017-10-14 20:13:10 +0300

    Добавил черновик класса Compositor, добавил простое движение персонажа

--- src/js/main.js
+++ src/js/main.js
@@ -21,13 +21,54 @@ function drawBackground(background, context, spriteSheet) {
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
+class Compositor {
+    constructor() {
+        // Массив функций
+        this.layers = [];
+    }
+
+    draw(context) {
+        this.layers.forEach(layer => {
+            layer(context);
+        });
+    }
+}
+
+function createBackgroundLayer(backgrounds, sprites) {
+    const buffer = document.createElement('canvas');
+    buffer.width = 256;
+    buffer.height = 240;
+
+    backgrounds.forEach(background => {
+        drawBackground(background, buffer.getContext('2d'), sprites);
+    });
+
+    return function drawBackgroundLayer(context) {
+        context.drawImage(buffer, 0, 0);
+    };
+}
+
 Promise.all([
     loadMarioSprite(),
     loadBackgroundSprites()
 ]).then(([marioSprite, backgroundSprites]) => {
-    level.backgrounds.forEach(background => {
-        drawBackground(background, context, backgroundSprites);
-    });
+    const comp = new Compositor();
 
-    marioSprite.draw('idle', context, 64, 64);
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+    comp.layers.push(backgroundLayer);
+
+    const pos = {
+        x: 64,
+        y: 64
+    }
+
+    function update() {
+        comp.draw(context);
+        marioSprite.draw('idle', context, pos.x, pos.y);
+        pos.x += 1;
+        pos.y += 1;
+        requestAnimationFrame(update);
+    }
+
+    update();
 });
