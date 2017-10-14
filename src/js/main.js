361083ab0ea764bac1ea336bbe6f132fbebda2d9
Dmitriy Batogov <hi@batogov.ru>
2017-10-14 17:13:37 +0300

    Обновил main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,35 @@
-import tiles from './../img/tiles.png';
+import SpriteSheet from './sprite-sheet';
+import { loadImage } from './loaders';
 
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
+import tiles from './../img/tiles.png';
+import level from './../levels/1-1.json';
+
+/**
+ * Функция рисует тайлы бэкграунда.
+ * @param {object} background Объект, который описывает тайлы бэкграунда.
+ * @param {object} context Контекст рисования.
+ * @param {SpriteSheet} spriteSheet Спрайт-лист с определёнными тайлами.
+ */
+function drawBackground(background, context, spriteSheet) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; x++) {
+            for (let y = y1; y < y2; y++) {
+                spriteSheet.drawTile(background.tile, context, x, y);
+            }
+        }
+    })
 }
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 loadImage(tiles).then(image => {
-    context.drawImage(image, 0, 0);
+    const spriteSheet = new SpriteSheet(image, 16, 16);
+
+    spriteSheet.define('ground', 0, 0);
+    spriteSheet.define('sky', 3, 23);
+
+    level.backgrounds.forEach(background => {
+        drawBackground(background, context, spriteSheet);
+    });
 })
